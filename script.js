0ccb80d56b916186b51b4687cdfeb2769c5573f2
DarkManfred <Bobulon1>
2013-08-07 10:02:41 +0100

    Contains Google Maps Javascript
    This includes all the code relating to the map and the markers.

---
+++ script.js
@@ -0,0 +1,28 @@
+var map;
+
+var myLatlng = new google.maps.LatLng(52.4508168, -1.930513499999961);
+
+function initialize() {
+  var mapOptions = {
+    zoom: 5,
+    center: new google.maps.LatLng(55.378051, -3.43597299999999),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  
+  var marker = new google.maps.Marker({
+	  position: myLatlng,
+	  map: map,
+	  animation: google.maps.Animation.DROP,
+	  title: 'University of Birmingham.'
+  });
+  
+  google.maps.event.addListener(marker, 'click', function() {
+	  map.setZoom(8);
+	  map.setCenter(marker.getPosition());
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
