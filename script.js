6173ab09c5864e0fcfbf33b775fd8d5c0b582d0f
DarkManfred <Bobulon1>
2013-08-08 11:05:25 +0100

    Making map markers work with slide effect

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 google.maps.visualRefresh = true;
 var map;
-
+var infowindow = null;
+   
 function initialize() {
   var mapOptions = {
     zoom: 6,
-    center: new google.maps.LatLng(54.8,-9.9),
+    center: new google.maps.LatLng(53.709714,-3.826675),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
    
    map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-  placeMarkers();
+   
+  // infowindow = new google.maps.InfoWindow({
+	//   content: "placeholder..."
+  // });
+	   
+
   }
   
    function placeMarkers() {
@@ -23,37 +29,25 @@ function initialize() {
               var marker = new google.maps.Marker({
 	              position: myLatLng,
 	              map: map,
-	              animation: google.maps.DROP,
+	              animation: google.maps.Animation.DROP,
 	              title: val.research_institution
 	           });
 	        });
 	      });
 	      
-	     
-  google.maps.event.addListener(marker, 'click', function() {
-	  map.setZoom(8);
-	  map.setCenter(marker.getPosition());
-  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function() {
-  $('form').submit(function() {  
-    $('#landing-slide').animate({"left":"80%"}, 1500);
+  $('form').submit(function(e2) {
+	  console.log("Working up to here");
+	  e2.preventDefault();
+    $('#landing-slide').animate({"left":"80%"}, 1500, function(){
+		placeMarkers();
+		});
   });
   $('#hide').click(function() {  
     $('#landing-slide').hide();
   });
-});
-
-$(function() {
-  $('form').on('submit', function(e) {
-      gMap = new google.maps.Map(document.getElementById('map-canvas')); 
-      gMap.setZoom(7);      // This will trigger a zoom_changed on the map
-      gMap.setCenter(new google.maps.LatLng(54.8,-2.3));
-      gMap.setMapTypeId(google.maps.MapTypeId.ROADMAP);
-      
-      e.preventDefault();
-    });
 });
