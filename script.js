c0000847861331236f9ea920ebbbce9b7ad7091f
DarkManfred <Bobulon1>
2013-08-09 10:58:53 +0100

    Adding InfoWindow Function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 google.maps.visualRefresh = true;
 var map;
+var infowindow = null;
    
 function initialize() {
   var mapOptions = {
@@ -11,49 +12,127 @@ function initialize() {
    map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 	   
-  }
-  
-  
+}
+
+function createHTML(result) {
+    HTMLtext = new Array();
+	$.each(result.establishments, function(ndx, val) {
+        var html = "<div id='content'><h2>";
+        html += val.orgname;
+        html += "</h2>";
+        html += "<div class='proj-info'>";
+        html += "<h3>";
+        html += val.projects[0].title;
+        html += "</h3>"
+        html += "<table border='1'>";
+        html += "<tr class='funds'>";
+        html += "<td>";
+        html += "<b>";
+        html += "Amount";
+        html += "</b>";
+        html += "</td>";
+        html += "<td>";
+        html += val.projects[0].amount;
+        html += "</td>";
+        html += "</tr>";
+        html += "<tr class='funded by'>";
+        html += "<td>";
+        html += "<b>";
+        html += "Funded By"
+        html += "</b>";
+        html += "</td>";
+        html += "<td>";
+        html += val.projects[0].funder;
+        html += "</td>";
+        html += "</tr>";
+        html += "<tr class='active'>";
+        html += "<td>";
+        html += "<b>";
+        html += "Project Status";
+        html += "</b>";
+        html += "</td>";
+        html += "<td>";
+        html += val.projects[0].status;
+        html += "</td>";
+        html += "</tr>";
+        html += "<tr class='start-date'>";
+        html += "<td>";
+        html += "<b>";
+        html += "Start Date";
+        html += "</b>";
+        html += "</td>";
+        html += "<td>";
+        html += val.projects[0].start;
+        html += "</td>"; 
+        html += "</tr>";
+        html += "<tr class='end-date'>";
+        html += "<td>";
+        html += "<b>";
+        html += "End Date";
+        html += "</b>";
+        html += "</td>";
+        html += "<td>";
+        html += val.projects[0].end;
+        html += "</td>";
+        html += "</tr>";
+        html += "<tr class='abstract'>";
+        html += "<td>";
+        html += "<b>";
+        html += "Abstract";
+        html += "</b>";
+        html += "</td>";
+        html += "<td>";
+        html += val.projects[0].abstract;
+        html += "</td>";
+        html += "</tr>";
+        html += "</table>";
+        html += "</div>";
+        html += "</div>";
+   		HTMLtext[ndx] = html;
+    });
+}
   
 function placeMarkers(result) {     
-	$.each(result.establishments, function(key, val) {
-         //alert(val.lng)
+	$.each(result.establishments, function(ndx, val) {
  	       var myLatLng = new google.maps.LatLng(val.lat, val.lng);
            var marker = new google.maps.Marker({
 	           position: myLatLng,
 	           map: map,
 	           animation: google.maps.Animation.DROP,
-	           title: val.orgname
+	           title: val.orgname,
+	           html: HTMLtext[ndx]
 	        });
-	 });
-	      
+	        
+	        var contentString = "Some content";
+	        
+	        google.maps.event.addListener(marker, 'click', function() {
+				infowindow.setContent(this.html);
+				infowindow.open(map, this);
+		    });
+	});	      
 }
-
-//function infoWindows(result) {
-	//$.each(result.
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
 $(document).ready(function() {
-    $('#loading').hide();
     $('form').submit(function(e2) {
 	    console.log("Working up to here");
 	    e2.preventDefault();
-      $('#loading').show();
-        $('#landing-slide').animate({left:'80%'}, 1000,'easeOutBounce', function(){
-          $('#landing-slide').animate({right:'80%'}, 1000,'easeOutBounce')
+        $('#landing-slide').animate({"left":"80%"}, 1500, function(){
 		    $.getJSON(
-            "/cgi-bin/everything.py",
-            {"term" : $('#input-text').val()},
+             "/cgi-bin/everything.py",
+           {"term" : $('#input-text').val()},
              function(result) { 
-              $('#loading').hide();
+		        createHTML(result);
 		        placeMarkers(result);
+		           infowindow = new google.maps.InfoWindow({
+	                  content: "loading..."
+		        
 	         });
-
 		});
     });
   $('#hide').click(function() {  
     $('#landing-slide').hide();
   });
+});
 });
